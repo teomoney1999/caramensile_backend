82af7da19f2c7c1c60246182ab0931ebd23dd493
Quoc Anh Truong <teomoney1999@gmail.com>
2023-11-27 21:51:36 +0700

    update

--- application/models/customer/transaction.py
+++ application/models/customer/transaction.py
@@ -8,15 +8,14 @@ class Delivery(CommonModel):
     destination = db.Column(String()) 
     distance = db.Column(DECIMAL) 
     # RESPONSE TIME
-    stared_at = db.Column(BigInteger()) 
+    # stared_at = db.Column(BigInteger()) 
+    started_at = db.Column(BigInteger()) 
     ended_at = db.Column(BigInteger()) 
     
     # FEES
     fee_customer_paid = db.Column(DECIMAL)
     fee_company_paid = db.Column(DECIMAL) 
     total_fee = db.Column(DECIMAL)
-    # 0: received order, 1: ready for delivery 2: on the road, 3: shipped
-    status = db.Column(SmallInteger(), index=True, default=0)  
     
     order = db.relationship("Order", back_populates="delivery") 
     order_id = db.Column(UUID(as_uuid=True), ForeignKey("orders.id"), index=True)
@@ -25,11 +24,15 @@ class Delivery(CommonModel):
 class Order(CommonModel): 
     __tablename__ = "orders"
     no = db.Column(Integer(), autoincrement=True, index=True) 
+    # STATUS
     payment_status = db.Column(SmallInteger(), index=True, default=0)   # 0: no paid, 1: paid
-    delivery_status = db.Column(SmallInteger(), index=True, default=0)  # 0: pending, 1: success, 2: failed
+    delivery_status = db.Column(SmallInteger(), index=True, default=0)  # 0: received order, 1: ready for delivery 2: on the road, 3: shipped
+    status = db.Column(SmallInteger(), index=True, default=0)  # 0: received, 1: ready, 2: shipping, 3: successful, 4: failed
+    
     # WHEN FINISHED ORDER
     completed_at = db.Column(BigInteger()) 
     paid_at = db.Column(BigInteger()) 
+    shipped_at = db.Column(BigInteger()) 
     
     # DELIVERY
     delivery_fee = db.Column(DECIMAL)
@@ -51,6 +54,9 @@ class Order(CommonModel):
 class OrderDetail(CommonModel): 
     __tablename__ = "order_details"
     quantity = db.Column(SmallInteger(), default=0) 
+    product_name = db.Column(String()) 
+    price = db.Column(DECIMAL, default=0)  
+    
     # TODO calculate "amount" if not calculate from FE (Event) 
     amount = db.Column(DECIMAL, default=0)
     
--- manage.py
+++ manage.py
@@ -1,4 +1,84 @@
 from application import run_app
+from application.database import db
+import sqlalchemy
+from sqlalchemy.inspection import inspect
+from application.extensions.apimanager.sqlalchemy.helpers import to_dict
+import json
 
-if __name__ == "__main__": 
+def generate_schema(path = "schema", exclude = None, prettyprint = True):
+    """ Generate javascript schema"""
+    exclude_list = None
+    if path is None:
+        print("Path is required")
+        return
+    
+    if exclude is not None:
+        exclude_list = exclude.split(",")
+    
+    
+    for cls in [cls.class_ for cls in db.Model.registry.mappers if isinstance(cls.class_, type) and issubclass(cls.class_, db.Model)]:
+        classname = cls.__name__
+        schema = {}
+        for col in cls.__table__.c:
+            col_type = str(col.type)
+            schema_type = ''
+            if 'DECIMAL' in col_type:
+                schema_type = 'number'
+            if col_type in ['INTEGER','SMALLINT', 'FLOAT', 'BIGINT' ]:
+                schema_type = 'number'
+            if col_type == 'DATETIME':
+                schema_type = 'datetime'
+            if col_type == 'DATE':
+                schema_type = 'datetime'
+            if 'VARCHAR' in col_type:
+                schema_type = 'string'
+            if col_type in ['VARCHAR', 'UUID', 'TEXT']:
+                schema_type = 'string'
+            if col_type in ['JSON', 'JSONB']:
+                schema_type = 'json'
+            if 'BOOLEAN' in col_type:
+                schema_type = 'boolean'
+            
+            schema[col.name] = {"type": schema_type}
+            
+            if col.primary_key:
+                schema[col.name]["primary"] = True
+            #nullabel
+            if (not col.nullable) and (not col.primary_key):
+                schema[col.name]["required"] = True
+                
+            if hasattr(col.type, "length") and (col.type.length is not None):
+                schema[col.name]["length"] = col.type.length
+            
+            #default
+            if (col.default is not None) and (col.default.arg is not None) \
+                and (not callable(col.default.arg)) and not isinstance(col.default.arg, sqlalchemy.sql.functions.GenericFunction):
+                #print(col.default, col.default.arg, callable(col.default.arg))
+                schema[col.name]["default"] = col.default.arg
+                
+            #User confirm_password
+            if (classname == "User") and ("password" in col.name):
+                schema["confirm_password"] = {"type": schema_type}
+                schema["confirm_password"]["length"] = col.type.length
+                
+                
+        
+        relations = inspect(cls).relationships
+        for rel in relations:
+            if rel.direction.name in ['MANYTOMANY', 'ONETOMANY']:
+                schema[rel.key] = {"type": "list"}
+            if rel.direction.name in ['MANYTOONE']:
+                schema[rel.key] = {"type": "dict"}
+        
+        if prettyprint:
+            with open(path + '/' + classname + 'Schema.json', 'w') as outfile:
+                json.dump(schema,  outfile, indent=4,)
+        else:
+            with open(path + '/' + classname + 'Schema.json', 'w') as outfile:
+                json.dump(schema,  outfile,)
+
+
+
+if __name__ == "__main__":
+    # generate_schema()
     run_app()
