345921f11861d885adade676aad199a63eb61b5c
Quoc Anh Truong <teomoney1999@gmail.com>
2023-12-06 22:35:24 +0700

    update few change on model

--- application/models/customer/transaction.py
+++ application/models/customer/transaction.py
@@ -74,7 +74,7 @@ class OrderDetail(CommonModel):
     order_id = db.Column(UUID(as_uuid=True), ForeignKey("orders.id"), index=True)
     
     product_id = db.Column(UUID(as_uuid=True), ForeignKey("products.id"), nullable=True)
-    product = db.relationship("Product", nullable=True)
+    product = db.relationship("Product")
     
 
     
